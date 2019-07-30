bac6993a14b65210b7b39b80ad752688157e4af4
thesecondbestcoder <jpark0521@gmail.com>
2019-07-31 02:11:03 +0900

    B D S M

---
+++ src/quadtree.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include	"__common.hpp"
+#include	"entity.hpp"
+#include	"component.hpp"
+#include	"hitbox.hpp"
+#include	"component.hpp"
+
+
+class Quadtree {
+	private:
+	int Max_Objects = 6; // max amount of objects in a node
+	int Current_Level;
+	std::vector<std::reference_wrapper<struct capsule&>> Object_List;
+	std::vector<int> nodes;
+
+public:
+	Quadtree() {	};
+
+	void clear() {
+		for (int i = 0, i < nodes.size, i++) {
+			
+			nodes[i] = NULL;
+		}
+	}
+};
+
