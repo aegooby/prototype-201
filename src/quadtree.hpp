913e6d6b9543cf6a558182253d7bcd258eb43db6
aegooby <aegooby@gmail.com>
2020-12-21 23:08:41 +0300

    Commented out some of that quadtree shit cuz it's broken good job Chinese man

--- src/quadtree.hpp
+++ src/quadtree.hpp
@@ -8,38 +8,42 @@
 namespace p201
 {
 
+struct node
+{
+    float x0; // leftern edge
+    float x1; // rightern edge
+    float y0; // bottom edge
+    float y1; // top edge
+
+    float subwidth() const
+    {
+        return x1 / 2.0f;
+    }
+    float subheight() const
+    {
+        return y1 / 2.0f;
+    }
+};
+inline bool operator==(const node& one, const node& two)
+{
+    return one.x0 == two.x0 && one.x1 == two.x1 && one.y0 == two.y0 &&
+           one.y1 == two.y1;
+}
+
 class quadtree
 {
-public:
-    struct node
-    {
-        float x0; // leftern edge
-        float x1; // rightern edge
-        float y0; // bottom edge
-        float y1; // top edge
-
-        float subwidth()
-        {
-            return x1 / 2.0f;
-        }
-        float subheight()
-        {
-            return y1 / 2.0f;
-        }
-    };
-
-private:
+protected:
     std::vector<std::vector<std::vector<std::reference_wrapper<circle>>>>
                                    object_list;
     std::vector<std::vector<node>> nodelist;
     std::vector<std::vector<int>>  subnode_num;
     std::pair<int, int>            level_subnode;
     int                            index;
-    node                           Basisknoten;
+    node                           base_node;
     int                            level = 0;
     void                           add_subnodes(int current_level)
     {
-        for (int i = 1; i < 5; i++) { subnode_num[current_level].push_back(i); }
+        for (int i = 1; i < 5; i++) subnode_num[current_level].push_back(i);
     }
     int  maxobj = 8;
     node bounds; // boundary of the current node
@@ -59,7 +63,7 @@ public:
     ~quadtree();
     void                 split(node&);
     int                  get_index(circle&, node&);
-    void                 insert(circle&, int&);
+    void                 insert(circle&, int);
     void                 recursive_insert(circle&, int);
     void                 collision_check(circle&);
     std::pair<int, int>& get_levelsubnode(circle&);
