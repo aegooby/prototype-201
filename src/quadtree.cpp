fd8ab4b43b83ec087551ee3c6c432551ecb68358
thesecondbestcoder <jpark0521@gmail.com>
2020-12-24 06:02:41 +0900

    quad shit unfinished

--- src/quadtree.cpp
+++ src/quadtree.cpp
@@ -9,7 +9,12 @@
 
 namespace p201
 {
-
+quadtree::quadtree(size_t threshold, size_t max_depth, size_t max_objects, class world& world){
+    _threshold = threshold;
+    _max_depth = max_depth;
+    _max_objects = max_objects;
+    
+};
 
 
 } // namespace p201
--- src/quadtree.hpp
+++ src/quadtree.hpp
@@ -8,86 +8,18 @@
 namespace p201
 {
 
-struct box
-{
-    float x;
-    float y;
-    float w;
-    float h;
-
-    constexpr box(float x, float y, float w, float h) noexcept
-        : x(x), y(y), w(w), h(h)
-    {
-    }
-    ~box() = default;
-
-    constexpr float right() const noexcept
-    {
-        return x + w;
-    }
-    constexpr float left() const noexcept
-    {
-        return x;
-    }
-    constexpr float bottom() const noexcept
-    {
-        return y + h;
-    }
-    constexpr float top() const noexcept
-    {
-        return y;
-    }
-    vector_2 center() const noexcept
-    {
-        return vector_2(x + w / 2.0f, y + h / 2.0f);
-    }
-    constexpr bool contains(const box& other) const noexcept
-    {
-        return left() <= other.left() && other.right() <= right() &&
-               top() <= other.top() && other.bottom() <= bottom();
-    }
-    constexpr bool intersects(const box& other) const noexcept
-    {
-        return !(left() >= other.right() || right() <= other.left() ||
-                 top() >= other.bottom() || bottom() <= other.top());
-    }
-};
-
-inline constexpr bool intersect(const box& one, const box& two) noexcept
-{
-    return !(one.left() >= two.right() || one.right() <= two.left() ||
-             one.top() >= two.bottom() || one.bottom() <= two.top());
-}
-
-template<typename type>
 class quadtree
 {
 private:
-    
-    
+    size_t _threshold;
+    size_t _max_depth;
+    size_t _max_objects;
+    std::vector<quadtree> nodes;
+       
 public:
-    struct node
-    {
-        std::array<std::unique_ptr<node>, 4> children;
-        std::vector<type>                    values;
-
-        bool leaf() const
-        {
-            return !static_cast<bool>(children.at(0));
-        }
-    };
-
-protected:
-    size_t threshold;
-    size_t max_depth;
-    size_t max_objects;
-
-    std::unique_ptr<node> root;
-
-public:
-    quadtree(size_t threshold, size_t max_depth, size_t max_objects);
+    quadtree(size_t threshold, size_t max_depth, size_t max_objects, world& world);
     ~quadtree() = default;
-    
+    void smash();
     
 };
 
