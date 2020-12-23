e250be4a0108f22eafef6ae0170319aca2055fd4
aegooby <aegooby@gmail.com>
2020-12-23 18:13:17 +0300

    Removed entity types

--- src/entity.cpp
+++ src/entity.cpp
@@ -19,9 +19,8 @@ std::unique_ptr<component>& entity::__component(std::type_index component_type)
 void entity::__add_component(std::unique_ptr<struct component>&& component,
                              std::type_index                     component_type)
 {
-    world.add_component(
-        *this, std::forward<std::unique_ptr<struct component>>(component),
-        component_type);
+    using ptr_t = std::unique_ptr<struct component>;
+    world.add_component(*this, std::forward<ptr_t>(component), component_type);
 }
 void entity::__remove_component(std::type_index component_type)
 {
