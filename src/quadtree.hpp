ad9e4a0e41ada5266c43071aab320423b2798105
thesecondbestcoder <jpark0521@gmail.com>
2020-12-24 20:53:21 +0900

    quaddddddddd

--- src/quadtree.hpp
+++ src/quadtree.hpp
@@ -3,7 +3,7 @@
 #include "__common.hpp"
 #include "component.hpp"
 #include "entity.hpp"
-#include "hitbox.hpp"
+#include "world.hpp"
 
 namespace p201
 {
@@ -14,13 +14,19 @@ private:
     size_t                _threshold;
     size_t                _max_depth;
     size_t                _max_objects;
-    std::vector<quadtree> nodes;
+    size_t                _width;
+    size_t                _height;
+    int                _x; // top left x position
+    int                _y; // top left y position
+    
+    std::unique_ptr<quadtree> nodes [4];
 
 public:
-    quadtree(size_t threshold, size_t max_depth, size_t max_objects,
-             world& world);
+    quadtree(size_t threshold, size_t max_depth, size_t max_objects, size_t width, size_t height, int x, int y);
     ~quadtree() = default;
-    void smash();
+    void add_nodes();
+    void populate();
+    void smashable();
 };
 
 } // namespace p201
