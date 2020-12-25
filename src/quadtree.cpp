5d600906ce2175bea711f5052574d16a0930b0ad
thesecondbestcoder <jpark0521@gmail.com>
2020-12-26 03:43:34 +0900

    quad

--- src/quadtree.cpp
+++ src/quadtree.cpp
@@ -5,11 +5,17 @@
 
 namespace p201
 {
+<<<<<<< HEAD
 quadtree::quadtree(size_t threshold, size_t max_depth, size_t max_objects, size_t width, size_t height, vector_3 position)
+=======
+quadtree::quadtree(size_t threshold, size_t max_depth, size_t max_objects,
+                   size_t width, size_t height, int x, int y)
+>>>>>>> 9dc9e8f2a30498a1ee579b46810be78c0f32466f
 {
     _threshold   = threshold;
     _max_depth   = max_depth;
     _max_objects = max_objects;
+<<<<<<< HEAD
     _width = width;
     _height = height;
     _position = position;
@@ -29,14 +35,37 @@ void quadtree::add_nodes() {
     
     nodes[3] = std::make_unique<quadtree>(_threshold, _max_depth-1, _max_objects, new_width, new_height, vector_3(_position(1) + new_width, _position(2) - new_height,0));
 }
+=======
+    _width       = width;
+    _height      = height;
+    _x           = x;
+    _y           = y;
 
-void quadtree::populate(){
-    
+    // width,height = 480, 360
+};
+
+void quadtree::add_nodes()
+{
+    nodes[0] =
+        std::make_unique<quadtree>(_threshold, _max_depth - 1, _max_objects,
+                                   _width / 2, _height / 2, _x + _width, _y);
+>>>>>>> 9dc9e8f2a30498a1ee579b46810be78c0f32466f
+
+    nodes[1] =
+        std::make_unique<quadtree>(_threshold, _max_depth - 1, _max_objects,
+                                   _width / 2, _height / 2, _x, _y);
+
+    nodes[2] =
+        std::make_unique<quadtree>(_threshold, _max_depth - 1, _max_objects,
+                                   _width / 2, _height / 2, _x, _y - _height);
+
+    nodes[3] = std::make_unique<quadtree>(_threshold, _max_depth - 1,
+                                          _max_objects, _width / 2, _height / 2,
+                                          _x + _width, _y - _height);
 }
 
-void quadtree::smashable(){
-    
-}
+void quadtree::populate() { }
 
+void quadtree::smashable() { }
 
 } // namespace p201
--- src/quadtree.hpp
+++ src/quadtree.hpp
@@ -9,6 +9,7 @@ namespace p201
 class quadtree
 {
 private:
+<<<<<<< HEAD
     size_t                _threshold;
     size_t                _max_depth;
     size_t                _max_objects;
@@ -20,6 +21,21 @@ private:
 
 public:
     quadtree(size_t threshold, size_t max_depth, size_t max_objects, size_t width, size_t height, vector_3 position);
+=======
+    size_t _threshold;
+    size_t _max_depth;
+    size_t _max_objects;
+    size_t _width;
+    size_t _height;
+    int    _x; // top left x position
+    int    _y; // top left y position
+
+    std::unique_ptr<quadtree> nodes[4];
+
+public:
+    quadtree(size_t threshold, size_t max_depth, size_t max_objects,
+             size_t width, size_t height, int x, int y);
+>>>>>>> 9dc9e8f2a30498a1ee579b46810be78c0f32466f
     ~quadtree() = default;
     void add_nodes();
     void populate();
