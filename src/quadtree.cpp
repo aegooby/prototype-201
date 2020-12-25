bbd20156bdf0ad2ed2b4bec7aa3f97aa68b97dda
thesecondbestcoder <jpark0521@gmail.com>
2020-12-26 03:41:39 +0900

    quuuaaaddd

--- src/quadtree.cpp
+++ src/quadtree.cpp
@@ -1,34 +1,33 @@
 
 #include "quadtree.hpp"
-
 #include "__common.hpp"
-#include "component.hpp"
-#include "entity.hpp"
-#include "world.hpp"
+#include "linalg.hpp"
 
 namespace p201
 {
-quadtree::quadtree(size_t threshold, size_t max_depth, size_t max_objects, size_t width, size_t height, int x, int y)
+quadtree::quadtree(size_t threshold, size_t max_depth, size_t max_objects, size_t width, size_t height, vector_3 position)
 {
     _threshold   = threshold;
     _max_depth   = max_depth;
     _max_objects = max_objects;
     _width = width;
     _height = height;
-    _x = x;
-    _y = y;
+    _position = position;
     
 // width,height = 480, 360
 };
 
 void quadtree::add_nodes() {
-    nodes[0] = std::make_unique<quadtree>(_threshold, _max_depth-1, _max_objects, _width/2, _height/2,_x + _width, _y);
+    int new_width = _width/2;
+    int new_height = _height/2;
     
-    nodes[1] = std::make_unique<quadtree>(_threshold, _max_depth-1, _max_objects, _width/2, _height/2,_x, _y);
+    nodes[0] = std::make_unique<quadtree>(_threshold, _max_depth-1, _max_objects, new_width, new_height, vector_3(_position(1) + new_width, _position(2), 0));
     
-    nodes[2] = std::make_unique<quadtree>(_threshold, _max_depth-1, _max_objects, _width/2, _height/2,_x, _y - _height);
+    nodes[1] = std::make_unique<quadtree>(_threshold, _max_depth-1, _max_objects, new_width, new_height, vector_3(_position(1),  _position(2), 0));
     
-    nodes[3] = std::make_unique<quadtree>(_threshold, _max_depth-1, _max_objects, _width/2, _height/2,_x + _width, _y - _height);
+    nodes[2] = std::make_unique<quadtree>(_threshold, _max_depth-1, _max_objects, new_width, new_height, vector_3(_position(1),  _position(2) - new_height),0);
+    
+    nodes[3] = std::make_unique<quadtree>(_threshold, _max_depth-1, _max_objects, new_width, new_height, vector_3(_position(1) + new_width, _position(2) - new_height,0));
 }
 
 void quadtree::populate(){
--- src/quadtree.hpp
+++ src/quadtree.hpp
@@ -1,9 +1,7 @@
 
 #pragma once
 #include "__common.hpp"
-#include "component.hpp"
-#include "entity.hpp"
-#include "world.hpp"
+#include "linalg.hpp"
 
 namespace p201
 {
@@ -16,13 +14,12 @@ private:
     size_t                _max_objects;
     size_t                _width;
     size_t                _height;
-    int                _x; // top left x position
-    int                _y; // top left y position
+    vector_3              _position; //top left x & y pos
     
     std::unique_ptr<quadtree> nodes [4];
 
 public:
-    quadtree(size_t threshold, size_t max_depth, size_t max_objects, size_t width, size_t height, int x, int y);
+    quadtree(size_t threshold, size_t max_depth, size_t max_objects, size_t width, size_t height, vector_3 position);
     ~quadtree() = default;
     void add_nodes();
     void populate();
