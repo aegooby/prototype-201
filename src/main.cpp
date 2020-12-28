fad8eefa4ac068121afaa47e9c3e52d5b61acd05
aegooby <aegooby@gmail.com>
2020-12-28 21:49:19 +0300

    Added working directory info

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,12 @@
 #include "__common.hpp"
 #include "engine.hpp"
 
+#include <filesystem>
+
 int main(int argc, char** argv)
 {
+    debug(std::cout << "Working directory: ");
+    debug(std::cout << std::filesystem::current_path() << std::endl);
     p201::engine engine = p201::engine("prototype 201");
     engine.start();
 
